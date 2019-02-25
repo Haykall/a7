6ea03d7d38b2c4f436c335c274f08c7949bddff8
gzhang21 <zhang21@uw.edu>
2019-02-25 15:41:51 -0800

    fix population amount

--- summary.R
+++ summary.R
@@ -1,5 +1,8 @@
 library(dplyr)
   data <- read.csv("WDVP Datasets - small countries are beautiful 6.51.37 PM.csv", stringsAsFactors = FALSE)
+  View(data)
+  data <- data[-c(1:4),]
+  data[data == "-"] <- NA
 summary <- function(dataset) {
   dataset <- dataset[-c(1:4),]
   dataset[dataset == "-"] <- NA
@@ -7,17 +10,20 @@ summary <- function(dataset) {
                        numerals = c("allow.loss", "warn.loss", "no.loss"))  
   ret <- list()
   ret$no_countries <- nrow(dataset)
-  max_pop <- dataset %>% filter(population == max(population))
+  max_pop <- dataset %>% filter(population == "5,000,000")
+  View(max_pop)
   ret$max_pop <- max_pop %>% pull(indicator)
-  min_pop <- dataset %>% filter(population == min(population))
+  ret$max_pop_amount <- max_pop %>% pull(population)
+  min_pop <- dataset %>% filter(population == "100,000")
   ret$min_pop <- min_pop %>% pull(indicator)
+  ret$min_pop_amount <- min_pop %>% pull(population)
   ret$max_GDP <- max(dataset$GDP, na.rm = TRUE)
   ret$min_GDP <- min(dataset$GDP, na.rm = TRUE)
   ret$mean_GDP <- mean(dataset$GDP, na.rm = TRUE)
-  GDP <- dataset %>% select(
+  max_GDP <- dataset %>% select(
     indicator, GDP) %>% arrange(
       desc(GDP)) %>% head(1)
-  ret$max_GDP_country <- GDP %>% pull(indicator)
+  ret$max_GDP_country <- max_GDP %>% pull(indicator)
   happy <- dataset %>% select(
     indicator, world.happiness.report.score) %>% arrange(
       desc(world.happiness.report.score))
@@ -26,22 +32,9 @@ summary <- function(dataset) {
   ret
 }
 test1 <- summary(data)
-
-data <- transform(data, indicator = as.numeric(indicator), population = as.numeric(population))
-View(data)
-y <- sum(data$GDP, na.rm = TRUE)
-z <- mean(data$population, na.rm = TRUE)
-is.na(data$GPD, na.rm=TRUE)
-sapply(data, mode)
 str(data)
-data <- type.convert(data, na.strings = "NA", as.is = TRUE, dec = ".",
-             numerals = c("allow.loss", "warn.loss", "no.loss"))
-data <- data[-c(1:4),]
-data[data == "-"] <- NA
-min(data$GDP, na.rm = TRUE)
-max
-z <- as.numeric(data$population)
-yeet <- data %>% select(indicator, world.happiness.report.score)
-yeet <- yeet %>% arrange(desc(world.happiness.report.score))
-
-
+min <- data %>% select(indicator, population) %>% arrange(desc(population))
+View(min)
+data <- data %>% mutate(pop = as.numeric(population))
+View(data)
+max(data$population)
