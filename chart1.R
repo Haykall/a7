dce3e0f31ba50a2f368c8c0c17d752fee8d2f629
Jessica Prasetyo <jessipras@D-10-18-246-255.dhcp4.washington.edu>
2019-02-26 17:45:11 -0800

    work

--- chart1.R
+++ chart1.R
@@ -14,32 +14,32 @@ my_plot <- function(file_csv) {
   dataset$education.expenditure <- 
     as.numeric(as.character(dataset$education.expenditure))
   
-  #filter dataset to show the countries with the lowest human development index
+  #filter dataset to show the countries with the highest human development index
   #along with their respective health expenditure and education expenditure
-  highest_hdi <- dataset %>%
+ lowest_hdi <- dataset %>%
     select(indicator, human.development.index, education.expenditure, 
            health.expenditure) %>%
     arrange(human.development.index) %>%
     filter(human.development.index < 1 & human.development.index > 0.3) %>%
     head(5)
 
-  #determine what information is displayed on the plot, what fills the bar
+  #determine what information is disploayed on the plot, what fills the bar
   # chart
-  high_plot <- highest_hdi %>% gather(
+  low_plot <- lowest_hdi %>% gather(
     key = expenditure, value = health.expenditure,
     -indicator, -human.development.index
   )
-  
+
   #plot the highest hdi countries, comparing health and education expenditure
-  high_final_plot <- ggplot(high_plot) + geom_col(
+  low_final_plot <- ggplot(low_plot) + geom_col(
     mapping = aes(x = indicator, y = health.expenditure, fill = expenditure),
     position = "dodge"
   ) + scale_color_brewer(palette = "Set3") + labs(
-    title = "Comparing health  and education expenditure on the 
-    countries with the top 5 highest HDI ",
+    title = "Comparing health  and education expenditure on the countries 
+    with the top 5 lowest HDI ",
     x = "Indicators", y = "Expenditure Scale"
   )
   
-  high_final_plot
+  low_final_plot
 }
 
